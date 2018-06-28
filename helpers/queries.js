a895dc630a4ef0dc4384e1f5a4dcd3e9a19bbc7b
Rachael Boddy <rachael@jigsaw.xyz>
2018-06-28 09:15:20 +0100

    added unit tests for queries

--- helpers/queries.js
+++ helpers/queries.js
@@ -2,32 +2,35 @@ let { Client } = require('pg');
 const connectionString = 'postgres://localhost:5432/postgres'
 const Promise = require('bluebird')
 
-const createEvent = async (userId, courseId, total, time) => {
-  if (userId === undefined || courseId === undefined || total === undefined || time === undefined) {
-    return Promise.reject(new Error('Empty parameter supplied'))
+module.exports = class Queries {
+  constructor(userId, courseId) {
+    this.userId = userId || undefined;
+    this.courseId = courseId || undefined;
   }
-  try {
-    console.log(Client)
-    const client = new Client({ connectionString });
-    console.log(client)
-    await client.connect();
-    await client.query(`INSERT INTO events VALUES (DEFAULT, ${userId}, ${courseId}, ${total}, ${time})`);
-    await client.end();
-  } catch (err) { throw err }
-}
-
-const retrieveEvents = async (userId, courseId) => {
-  if (userId === undefined || courseId === undefined) {
-    return Promise.reject(new Error('Empty parameter supplied'))
+  async createEvent(total, time) {
+    if (this.userId === undefined || this.courseId === undefined || total === undefined || time === undefined) {
+      return Promise.reject(new Error('Empty parameter supplied'))
+    }
+    try {
+      const client = new Client({ connectionString });
+      await client.connect();
+      const res = await client.query(`INSERT INTO events VALUES (DEFAULT, ${this.userId}, ${this.courseId}, ${total}, ${time})`);
+      await client.end();
+      return res
+    } catch (err) { throw err }
   }
-  try {
-    const client = new Client({ connectionString });
-    await client.connect();
-    const res = await client.query(`SELECT AVG(total), SUM(time) FROM EVENTS WHERE user_id=${userId} and course_id=${courseId}`)
-    await client.end();
-    return res.rows[0]
-  } catch (err) { throw err }
-}
 
-module.exports = { createEvent, retrieveEvents }
+  async retrieveEvents() {
+    if (this.userId === undefined || this.courseId === undefined) {
+      return Promise.reject(new Error('Empty parameter supplied'))
+    }
+    try {
+      const client = new Client({ connectionString });
+      await client.connect();
+      const res = await client.query(`SELECT AVG(total), SUM(time) FROM EVENTS WHERE user_id=${this.userId} and course_id=${this.courseId}`)
+      await client.end();
+      return res.rows[0]
+    } catch (err) { throw err }
+  }
+}
 
--- test/queries.test.js
+++ test/queries.test.js
@@ -1,10 +1,12 @@
 const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 const rewire = require('rewire')
-const queries = require('./../helpers/queries')
-const sinon = require('sinon')
+let Queries = require('./../helpers/queries')
 const Promise = require('bluebird')
 
+chai.should();
 const { expect } = chai;
+chai.use(chaiAsPromised);
 
 const mockClient = class mockClient {
   constructor() { }
@@ -12,40 +14,39 @@ const mockClient = class mockClient {
   end() { return {}; }
   query(sql) {
     if (sql.indexOf('INSERT') !== -1) {
-      return {}
+      return Promise.resolve('Data Inserted')
     } else
-      return {
+      return Promise.resolve({
         rows: [{ avg: 10, sum: 50 }]
-      };
+      });
   }
 };
 
-describe('Queries helper functions', () => {
-  let client;
+const userId = 1;
+const courseId = 1;
+
+describe.only('Queries helper functions', () => {
+  let queries;
   describe('create event missing parameter', () => {
+    before(() => { queries = new Queries(userId, courseId) })
     it('should return appropriate error response', () => {
-      return queries.createEvent(1, 2)
+      return queries.createEvent(1)
         .catch((err) => {
           expect(err.message).to.eql('Empty parameter supplied');
           expect(err).to.be.instanceOf(Error);
         })
     })
   })
-  describe.only('create event missing parameter', () => {
+  describe('create event', () => {
     before(() => {
-      // Queries = rewire('./../helpers/queries');
-      // Queries.__set__('Client', mockClient);
-      sinon.stub(client, 'query').returns(Promise.resolve('boo'))
+      Queries = rewire('./../helpers/queries');
+      Queries.__set__('Client', mockClient);
+      queries = new Queries(userId, courseId)
     })
-    it('should return appropriate error response', () => {
-      return queries.createEvent(1, 1, 1, 1)
-        .then((res) => { console.log(res) })
-        .catch((err) => {
-          console.log(err)
-        })
-    })
+    it('should return success message', () => queries.createEvent(1, 1).should.eventually.eql('Data Inserted'))
   })
   describe('retrieve events missing parameter', () => {
+    before(() => { queries = new Queries(userId, courseId) })
     it('should return appropriate error response', () => {
       return queries.retrieveEvents()
         .catch((err) => {
@@ -54,5 +55,12 @@ describe('Queries helper functions', () => {
         })
     })
   })
-
+  describe('retrieve events', () => {
+    before(() => {
+      Queries = rewire('./../helpers/queries');
+      Queries.__set__('Client', mockClient);
+      queries = new Queries(userId, courseId)
+    })
+    it('should return summary data', () => queries.retrieveEvents(1, 1).should.eventually.eql({ avg: 10, sum: 50 }))
+  })
 })
